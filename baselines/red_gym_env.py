d23895f16139e437d31401dce3c9ac9db796dd9f
Jonayb <40180069+Jonayb@users.noreply.github.com>
2023-12-04 13:25:10 +0100

    Pass START_BUTTON action

--- baselines/red_gym_env.py
+++ baselines/red_gym_env.py
@@ -113,7 +113,7 @@ class RedGymEnv(Env):
             
         self.reset()
 
-    def reset(self, seed=None):
+    def reset(self, seed=None, options = None):
         self.seed = seed
         # restart game, skipping credits
         with open(self.init_state, "rb") as f:
@@ -190,6 +190,8 @@ class RedGymEnv(Env):
         return game_pixels_render
     
     def step(self, action):
+        if action == 6:
+            action = 7
         self.run_action_on_emulator(action)
         self.append_agent_stats(action)
 
